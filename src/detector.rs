c9d93ad446ddc9c8415261697d44b6133bc557b9
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2026-09-10 00:08:39 +0200

    Add face detector with Seeta model

---
+++ src/detector.rs
@@ -0,0 +1,11 @@
+use std::io::Cursor;
+use rustface::{read_model, Detector, create_detector_with_model};
+
+/// seeta Face embedded model
+const MODEL_BYTES: &[u8] = include_bytes!("../models/seeta_fd_frontal_v1.0.bin");
+
+pub fn build_detector() -> Result<Box<dyn Detector>, String> {
+    let model = read_model(Cursor::new(MODEL_BYTES)).map_err(|e| e.to_string())?;
+
+    Ok(create_detector_with_model(model))
+}
